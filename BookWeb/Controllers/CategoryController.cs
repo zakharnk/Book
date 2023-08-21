3eb1222524e7877bf4b9b99d460fce6fb53544bd
Anton Bohdan <antonbohdan@rovensys.com>
2023-08-21 13:28:12 +0200

    init book app

---
+++ BookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
